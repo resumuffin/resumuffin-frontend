dd8646b65bc3dcae7b248ba0422415f82a31d8fe
Chris Mayers <chrisandrewmayers@gmail.com>
2019-11-11 19:17:00 -0800

    adding upload component

---
+++ src/app/upload/upload.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.scss']
+})
+export class UploadComponent implements OnInit {
+
+  constructor() { }
+
+  url: string | ArrayBuffer;
+
+  isVisible = false;
+  
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+      }
+
+      this.isVisible = true;
+
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}

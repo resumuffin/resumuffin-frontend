b3527ec19cae6440cc8f6b9182214827faff856d
Connor Hale <connor.a.h.h@gmail.com>
2019-12-05 13:39:10 -0800

    added tags service and http request to populate

--- src/app/resume-service.service.ts
+++ src/app/resume-service.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResumeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   id = "";
-
+  // getTenResumes(){
+  //   return this.http.get('http://class-a.hekiyou.academy:9917/tag/get/all')
+  // }
   setId(id)
   {
     this.id = id;

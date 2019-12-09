1f624fd5496423ca924485adc84de9724a8b3d05
Chris Mayers <chrisandrewmayers@gmail.com>
2019-12-08 19:50:13 -0800

    adding redirect on upload

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-help',
@@ -8,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HelpComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }

493e7f2003c752ed25e3ff920369db4f4e41f576
Chris Mayers <chrisandrewmayers@gmail.com>
2019-12-12 12:47:02 -0800

    adding admin ability to delete resumes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import {
   ResumeService
 } from '../resume-service.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +14,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  deleteResumeURL = "http://springuserandcomments-env.sfredvy8k7.us-west-1.elasticbeanstalk.com/resume/delete/";
+  isAdmin;
   resumes: Object
-  constructor(private resumeService: ResumeService, private router: Router) {}
+  constructor(private resumeService: ResumeService, private router: Router, private http: HttpClient) {}
   ngOnInit() {
+    if (localStorage.getItem("IS_ADMIN"))
+    {
+      this.isAdmin = true;
+    }
+    this.setUpPage();
+  }
+  setUpPage(){
     this.resumeService.getAllResumes().subscribe(data => {
       this.resumes = data
       console.log(this.resumes);
@@ -28,6 +38,16 @@ export class HomeComponent implements OnInit {
       this.router.navigate(['../discussion']);
     },
     1500);
+  }
+  deleteResume(event){
+    var id = event.target.value;
+    var deleteResumeURLID = this.deleteResumeURL + id;
+    this.http.delete<any>(deleteResumeURLID, {withCredentials: true}).subscribe(
+      (data) => {
+        console.log(data);
+        this.setUpPage();
+      }
+    );
   }
 
 }
